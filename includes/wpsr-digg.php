5e7f02e17dfdd1b5b064d80441404d3e9b103cc1
vaakash <vaakash@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-30 02:33:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-socializer/tags/2.4@538052 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/wpsr-digg.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ * Digg buttons Processor code for WP Socializer Plugin
+ * Version : 1.0
+ * Author : Aakash Chakravarthy
+*/
+
+function wpsr_digg_script(){
+	// Return the script
+	return "\n<!-- WP Socializer - Digg Script -->\n" .
+	"<script type=\"text/javascript\">
+	(function() {
+		var s = document.createElement('SCRIPT'), s1 = document.getElementsByTagName('SCRIPT')[0];
+		s.type = 'text/javascript';
+		s.async = true;
+		s.src = 'http://widgets.digg.com/buttons.js';
+		s1.parentNode.insertBefore(s, s1);
+	})();
+	</script>" .
+	"\n<!-- WP Socializer - End Digg Script -->\n";
+}
+
+function wpsr_digg_bt_used(){
+
+	## Get template data
+	$wpsr_template1 = get_option('wpsr_template1_data');
+	$wpsr_template2 = get_option('wpsr_template2_data');
+	
+	$wpsr_template1_content = $wpsr_template1['content'] . $wpsr_template2['content'];
+	$is_diggbt_used = strpos($wpsr_template1_content, '{digg-bt}');
+
+	if ($is_diggbt_used === false) {
+		return 0;
+	} else {
+		return 1;
+	}
+	
+}
+
+function wpsr_digg($args = ''){
+
+	global $post;
+	
+	$details = wpsr_get_post_details();
+	$def_url = $details['permalink'];
+	$def_title = $details['title'];
+
+	$defaults = array (
+		'output' => 'button',
+ 		'url' => $def_url,
+ 		'title' => $def_title,
+		'type' => 'DiggMedium',
+		'text' => __('Digg this' ,'wpsr'),
+		'image' => WPSR_PUBLIC_URL . 'buttons/digg-bt.png',
+		'params' => '',
+	);
+	
+	$args = wp_parse_args($args, $defaults);
+	extract($args, EXTR_SKIP);
+	
+	$digg_processed = "\n<!-- Start WP Socializer Plugin - Digg Button -->\n";
+	
+	switch($output){
+		// Display the ordinary button
+		case 'button':
+			$digg_processed .= '<a class="DiggThisButton ' . $type . '" href="http://digg.com/submit?url=' . urlencode($url) . '&amp;title=' . urlencode($title) . '" ' . $params . '></a>';
+		break;
+		
+		// Display the Image format
+		case 'image':
+			$digg_processed .= '<a href="http://digg.com/submit?url=' . urlencode($url) . '&amp;title=' . urlencode($title) . '" ' . $params . '><img src="' . $image . '" alt="Digg!"  /></a>';
+		break;
+		
+		// Display the Text format
+		case 'text':
+			$digg_processed .= '<a href="http://digg.com/submit?url=' . urlencode($url) . '&amp;title=' . urlencode($title) . '" ' . $params . '>' . $text . '</a>';
+		break;
+	}
+	
+	$digg_processed .= "\n<!-- End WP Socializer Plugin - Digg Button -->\n";
+	
+	return $digg_processed;
+}
+
+function wpsr_digg_bt(){
+	## Digg Options
+	$wpsr_digg = get_option('wpsr_digg_data');
+
+	## Start Output
+	$wpsr_digg_bt_processed = wpsr_digg(array(
+		'output' => 'button',
+		'type' => $wpsr_digg['type'],
+	));
+	## End Output
+	
+	return $wpsr_digg_bt_processed;
+}
+
+function wpsr_digg_rss_bt(){
+
+	## Start Output
+	$wpsr_digg_processed = wpsr_digg(array(
+		'output' => 'text',
+		'params' => 'target="_blank"',
+	));
+	## End Output
+	
+	return $wpsr_digg_processed;
+}
+?>
---
+++ includes/wpsr-google.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Google +1 button Processor code for WP Socializer Plugin
+ * Version : 3.0
+ * Author : Aakash Chakravarthy
+*/
+
+function wpsr_plusone_script(){
+	// Return the script
+	return "\n<!-- WP Socializer - +1 Script -->\n".
+	'<script type="text/javascript" src="http://apis.google.com/js/plusone.js"></script>'.
+	"\n<!-- WP Socializer - End +1 Script -->\n";
+}
+
+function wpsr_plusone_bt_used(){
+
+	## Get template data
+	$wpsr_template1 = get_option('wpsr_template1_data');
+	$wpsr_template2 = get_option('wpsr_template2_data');
+	
+	$wpsr_template_content = $wpsr_template1['content'] . $wpsr_template2['content'];
+	$is_plusonebt_used = strpos_arr($wpsr_template_content, array('{plusone-small}', '{plusone-medium}', '{plusone-standard}', '{plusone-tall}'));
+
+	if ($is_plusonebt_used === false) {
+		return 0;
+	} else {
+		return 1;
+	}
+	
+}
+
+function wpsr_plusone($args = ''){
+	global $post;
+	
+	$details = wpsr_get_post_details();
+	$def_url = $details['permalink'];
+	$def_title = $details['title'];
+
+	$defaults = array(
+		'output' => 'button',
+ 		'url' => $def_url,
+ 		'title' => $def_title,
+		'type' => 'standard',
+		//'text' => __('+1 this' ,'wpsr'),
+		//'image' => WPSR_PUBLIC_URL . 'buttons/plusone-bt.png',
+		//'params' => '',
+	);
+	
+	$args = wp_parse_args($args, $defaults);
+	extract($args, EXTR_SKIP);
+	
+	$plusone_processed = "\n<!-- Start WP Socializer Plugin - +1 Button -->\n";
+	
+	switch($output){
+		// Display the ordinary button
+		case 'button':
+			$plusone_processed .= '<g:plusone size="' . $type . '" href="' . $url . '" ' . $params . '></g:plusone>';
+		break;
+	}
+	
+	$plusone_processed .= "\n<!-- End WP Socializer Plugin - +1 Button -->\n";
+	
+	return $plusone_processed;
+}
+
+function wpsr_plusone_bt($type = 'standard'){
+
+	## Start Output
+	$wpsr_plusone_bt_processed = wpsr_plusone(array(
+		'output' => 'button',
+		'type' => $type
+	));
+	## End Output
+	
+	return $wpsr_plusone_bt_processed;
+}
+
+function wpsr_plusone_rss_bt(){
+
+	## Start Output
+	$wpsr_plusone_processed = '';
+	## End Output
+	
+	return $wpsr_plusone_processed;
+}
+?>
